18b5ff124639d0c3781677dcb56916ff697c4224
luoxuwei <xuwei.luo@nio.com>
2021-03-22 18:21:20 +0800

    定义对象基类

---
+++ pythonvm/object/JObject.h
@@ -0,0 +1,14 @@
+//
+// Created by Xuwei Luo 罗旭维 on 2021/3/22.
+//
+
+#ifndef PYTHONVM_JOBJECT_H
+#define PYTHONVM_JOBJECT_H
+
+//python中所有数据都是对象，字符串/整数/浮点数/函数/模块/类定义等，它们都有共同的基类object。
+class JObject {
+
+};
+
+
+#endif //PYTHONVM_JOBJECT_H
