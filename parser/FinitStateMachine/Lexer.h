a613cd5ac6895f48408e536919482757b4181836
luoxuwei <xwluo@foxmail.com>
2021-07-02 17:46:11 +0800

    完成词法解析简单实现

--- parser/FinitStateMachine/Lexer.h
+++ parser/FinitStateMachine/Lexer.h
@@ -15,7 +15,14 @@ class Lexer {
 private:
     char *buf = NULL;
     size_t buf_len = 0;
+    int next=0;              //指向当前要读入的字符位置
+    int cur_start = 0;     //当前被词法分析器分析的字符串位置
+    int cur_end = 0;       //当前被词法分析器分析的字符串结束位置
+    int pre_start = 0;     //上一个被词法分析器分析的字符串起始位置
+    int pre_lineno = 0;    //上一个被词法分析器分析的字符串所在的行号
+    int pre_len = 0;       //上一个被词法分析器分析的字符串长度
 
+    int lineno = 1;        //
 
 public:
     Lexer(char *content, size_t len) {
@@ -23,51 +30,77 @@ public:
             return;
         }
         buf = new char[len];
+        buf_len = len;
         memcpy(buf, content, len);
     }
 
     ~Lexer() {
         if (buf != NULL) {
             delete buf;
+            buf = NULL;
         }
     }
 
     char * text() {
-
+        int len = length();
+        char *res = new char[len];
+        memcpy(res, buf + cur_start, len);
+        return res;
     }
 
     int length() {
-
+        return cur_end - cur_start;
     }
 
     char * pre_text() {
+        int len = pre_length();
+        char *res = new char[len];
+        memcpy(res, buf + pre_start, len);
+        return res;
+    }
 
+    int pre_length() {
+        return cur_start - pre_start;
     }
 
     char advance() {
-
+        if (next >= buf_len) {
+            return 0;
+        }
+        if (buf[next] == '\n') {
+            lineno++;
+        }
+        return buf[next++];
     }
 
     int mark_start() {
-
+        cur_end = cur_start = next;
+        return cur_start;
     }
 
     int mark_end() {
-
+        cur_end = next;
+        return cur_end;
     }
 
     int to_mark() {
-
+        next = cur_end;
+        return next;
     }
 
     int mark_prev() {
-
+        pre_start = cur_start;
+        pre_lineno = lineno;
+        return pre_start;
     }
 
     char lookahead(int n) {
+        if (next + n -1 >= buf_len) {
+            return 0;
+        }
 
+        return buf[next + n -1];
     }
-
 };
 
 
