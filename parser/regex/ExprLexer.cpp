6387ccc84661ea3daa040384da7f6ff30e7995ee
luoxuwei <xwluo@foxmail.com>
2021-07-04 20:01:11 +0800

    开发正则表达式词法解析逻辑

--- parser/regex/ExprLexer.cpp
+++ parser/regex/ExprLexer.cpp
@@ -98,3 +98,149 @@ bool ExprLexer::checkInQuoted(std::string &macroContent, int curlyBracesBegin, i
     }
     return inQuoted;
 }
+
+ExprLexer::Token ExprLexer::advance() {
+    if (currentToken == Token::EOS) {
+        //一个正则表达式解析结束后读入下一个表达式
+        if (exprCount >= regexExpr.size()) {
+            currentToken = Token::END_OF_INPUT;
+            return currentToken;
+        } else {
+            curExpr = &regexExpr.at(exprCount);
+            exprCount++;
+        }
+    }
+
+    if (charIndex >= curExpr->length()) {
+        currentToken = Token::EOS;
+        charIndex = 0;
+        return currentToken;
+    }
+
+    char c = curExpr->at(charIndex);
+    if (c == '"') {
+        inQuoted = !inQuoted;
+        charIndex++;
+        c = curExpr->at(charIndex);
+    }
+
+    sawEsc = (c == '\\');
+    if (sawEsc && curExpr->at(charIndex + 1) != '"' && !inQuoted) {
+        lexeme = handleEsc();
+    } else {
+        if (sawEsc && curExpr->at(charIndex + 1) == '"') {
+            lexeme = '"';
+            charIndex += 2;
+        } else {
+            lexeme = c;
+            charIndex++;
+        }
+    }
+
+    currentToken = (inQuoted || sawEsc)? Token::L : tokenMap[c];
+}
+
+int ExprLexer::handleEsc() {
+    /*当转移符 \ 存在时，它必须与跟在它后面的字符或字符串一起解读
+     *我们处理的转义字符有以下几种形式
+     * \b backspace
+     * \f formfeed
+     * \n newline
+     * \r carriage return 回车
+     * \s space 空格
+     * \t tab
+     * \e ASCII ESC ('\033')
+     * \DDD 3位八进制数
+     * \xDDD 3位十六进制数
+     * \^C C是任何字符， 例如^A, ^B 在Ascii 表中都有对应的特殊含义
+     * ASCII 字符表参见：
+     * http://baike.baidu.com/pic/%E7%BE%8E%E5%9B%BD%E4%BF%A1%E6%81%AF%E4%BA%A4%E6%8D%A2%E6%A0%87%E5%87%86%E4%BB%A3%E7%A0%81/8950990/0/9213b07eca8065387d4c671896dda144ad348213?fr=lemma&ct=single#aid=0&pic=9213b07eca8065387d4c671896dda144ad348213
+     */
+
+    //跳过'\'
+    charIndex++;
+    char c = std::toupper(curExpr->at(charIndex));
+    int rval = 0;
+    switch (c) {
+        case '\0' :
+            rval = '\\';
+            break;
+        case 'B':
+            rval = '\b';
+            break;
+        case 'F':
+            rval = '\f';
+            break;
+        case 'N' :
+            rval = '\n';
+            break;
+        case 'R' :
+            rval = '\r';
+            break;
+        case 'S':
+            rval = ' ';
+            break;
+        case 'T':
+            rval = '\t';
+            break;
+        case 'E' :
+            rval = '\033';
+            break;
+        case '^':
+            charIndex++;
+            /*
+             * 因此当遇到^后面跟在一个字母时，表示读入的是控制字符
+             * ^@ 在ASCII 表中的数值为0，^A 为1, 字符@在ASCII 表中数值为80， 字符A在ASCII表中数值为81
+             * 'A' - '@' 等于1 就对应 ^A 在 ASCII 表中的位置
+             * 具体可参看注释给出的ASCII 图
+             *
+             */
+            rval = (char) (std::toupper(curExpr->at(charIndex)) - '@');
+            break;
+        case 'X':
+             /*
+              * \X 表示后面跟着的三个字符表示八进制或十六进制数
+              */
+            charIndex++; //越过X
+            c = curExpr->at(charIndex);
+            if (isHexDigit(c)) {
+                rval = hex2Bin(c);
+                charIndex++;
+                c = curExpr->at(charIndex);
+            }
+
+            if (isHexDigit(c)) {
+                rval << 4;
+                rval += hex2Bin(c);
+                charIndex++;
+                c = curExpr->at(charIndex);
+            }
+
+            if (isHexDigit(c)) {
+                rval << 4;
+                rval += hex2Bin(c);
+            }
+            break;
+        default:
+            if (isOctDigit(c)) {
+               rval = oct2Bin(curExpr->at(charIndex));
+               charIndex++;
+               if (isOctDigit(curExpr->at(charIndex))) {
+                   rval << 3;
+                   rval += oct2Bin(curExpr->at(charIndex));
+                   charIndex++;
+               }
+                if (isOctDigit(curExpr->at(charIndex))) {
+                    rval << 3;
+                    rval += oct2Bin(curExpr->at(charIndex));
+                }
+            } else {
+                rval = c;
+            }
+
+
+    }
+    charIndex++;
+    return rval;
+
+}
--- parser/regex/ExprLexer.h
+++ parser/regex/ExprLexer.h
@@ -6,34 +6,67 @@
 #define REGEX_EXPRLEXER_H
 #include <vector>
 #include "MacroLexer.h"
-
+#define ASCII_COUNT 128
 class ExprLexer {
-private:
-    std::vector<std::string> regexExpr;
-    MacroLexer* macroLexer;
 
-    /*
-     * 对正则表达式进行预处理，将表达式中的宏进行替换，例如
-     * D*\.D 预处理后输出
-     * [0-9]*\.[0-9]
-     * 注意，宏是可以间套的，所以宏替换时要注意处理间套的情形
-     */
-    void preProcessExpr(const std::string& regex);
-    std::string & extracMacroNameFromInput(LexerBuffer &lexer);
-    std::string expandMacro(const std::string &name);
-    bool checkInQuoted(std::string &macroContent, int curlyBracesBegin, int curlyBracesEnd);
 public:
-    enum Type {aaa, bbb};
+    enum Token {
+        EOS, //正则表达式末尾
+        ANY,     // . 通配符
+        AT_BOL,  //^ 开头匹配符
+        AT_EOL,  //$ 末尾匹配符
+        CCL_END,  //字符集类结尾括号 ]
+        CCL_START,  //字符集类开始括号 [
+        CLOSE_CURLY, // }
+        CLOSE_PAREN,  //)
+        CLOSURE,      //*
+        DASH,       // -
+        END_OF_INPUT,  //输入流结束
+        L,        //字符常量
+        OPEN_CURLY, // {
+        OPEN_PAREN, // (
+        OPTIONAL,  //?
+        OR,       // |
+        PLUS_CLOSE
+    };
 
     ExprLexer(const char * macroFilePath, const std::string& regex) {
         macroLexer = new MacroLexer(macroFilePath);
         //扩展宏
         preProcessExpr(regex);
+
+        for (int i=0; i<ASCII_COUNT; i++) {
+            tokenMap[i] = Token::L;
+        }
+        tokenMap['.'] = Token::ANY;
+        tokenMap['^'] = Token::AT_BOL;
+        tokenMap['$'] = Token::AT_EOL;
+        tokenMap[']'] = Token::CCL_END;
+        tokenMap['['] = Token::CCL_START;
+        tokenMap['}'] = Token::CLOSE_CURLY;
+        tokenMap[')'] = Token::CLOSE_PAREN;
+        tokenMap['*'] = Token::CLOSURE;
+        tokenMap['-'] = Token::DASH;
+        tokenMap['{'] = Token::OPEN_CURLY;
+        tokenMap['('] = Token::OPEN_PAREN;
+        tokenMap['?'] = Token::OPTIONAL;
+        tokenMap['|'] = Token::OR;
+        tokenMap['+'] = Token::PLUS_CLOSE;
     }
 
     ~ExprLexer() {
         delete macroLexer;
     }
+
+    bool matchToken(Token t) {
+        return currentToken == t;
+    }
+
+    int getLexeme() {
+        return lexeme;
+    }
+
+    Token advance();
 
     void printMacs() {
         macroLexer->printMacs();
@@ -47,6 +80,68 @@ public:
             });
         }
     }
+
+    int getRegularExpressionCount() {
+        return regexExpr.size();
+    }
+
+    const char * getRegularExpression(int index) {
+        if (index < 0 || index >= regexExpr.size()) {
+            return NULL;
+        }
+
+        return regexExpr.at(index).c_str();
+    }
+
+private:
+    std::vector<std::string> regexExpr;
+    MacroLexer* macroLexer;
+    Token currentToken = Token::EOS;
+    int exprCount = 0;
+    std::string *curExpr;
+    int charIndex = 0;
+    bool inQuoted = false; //是否在双引号内
+    bool sawEsc = false;  //是否读取到转移符 /
+    int lexeme ;
+    Token tokenMap[ASCII_COUNT] = {Token::L};
+    /*
+     * 对正则表达式进行预处理，将表达式中的宏进行替换，例如
+     * D*\.D 预处理后输出
+     * [0-9]*\.[0-9]
+     * 注意，宏是可以间套的，所以宏替换时要注意处理间套的情形
+     */
+    void preProcessExpr(const std::string& regex);
+    std::string & extracMacroNameFromInput(LexerBuffer &lexer);
+    std::string expandMacro(const std::string &name);
+    bool checkInQuoted(std::string &macroContent, int curlyBracesBegin, int curlyBracesEnd);
+
+    int handleEsc();
+    bool isHexDigit(char c) {
+        return (isdigit(c) || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F'));
+    }
+
+    bool isOctDigit(char c) {
+        return ('0' <= c && c <= '7');
+    }
+
+    int hex2Bin(char c) {
+        /*
+        * 将十六进制数对应的字符转换为对应的数值，例如
+        * A 转换为10， B转换为11
+        * 字符c 必须满足十六进制字符： 0123456789ABCDEF
+        */
+        return (isdigit(c)? c - '0': (std::toupper(c) - 'A' + 10)) & 0xf;
+    }
+
+    int oct2Bin(char c) {
+        /*
+         * 将字符c 转换为对应的八进制数
+         * 字符c 必须是合法的八进制字符: 01234567
+         */
+        return ((c) - '0') & 0x7;
+    }
+
+
 };
 
 
