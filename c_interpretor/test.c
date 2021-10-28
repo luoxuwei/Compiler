8965c198d8b3c003f0a0eba0012494848c7902c4
luoxuwei <xwluo@foxmail.com>
2021-10-28 22:56:07 +0800

    解释执行间套结构体

--- c_interpretor/test.c
+++ c_interpretor/test.c
@@ -1,13 +1,14 @@
 void main() {
-    struct TAG {
+    struct s1 {
         int v1;
+    } ;
+    struct s2 {
+        struct s1 s;
         int v2;
-        char v3;
-    } tag;
+    } ;
 
-    struct TAG myTag;
-    struct TAG herTag;
-    myTag.v1 = 1;
-    herTag.v1 = 2;
-    printf("set filed v1 of struct myTag to value : %d, and v1 of herTag to value : %d", myTag.v1, herTag.v1);
+    struct s2 tag;
+    tag.s.v1 = 1;
+    tag.v2 = 2;
+    printf("set filed v1 of struct s1 to value : %d, and v2 of tag to value : %d", tag.s.v1, tag.v2);
 }
